298b036000a20253b996a22ce81b0bec0d1edfe2
Yashwanth <yashwanth.t@hotmail.com>
2020-05-28 01:37:12 +0530

    Adding additional actual test case which yet to make pass.

--- NumberSpell.Test/TestNumberSpell.cs
+++ NumberSpell.Test/TestNumberSpell.cs
@@ -18,5 +18,18 @@ namespace NumberSpell.Test
             //Assert
             Assert.IsNotNull(spell);           
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int testData = 1234;
+
+            //Act
+            string spell = SpellNumber.GetSpell(testData);
+
+            //Assert
+            Assert.AreEqual("one thousand two hundred thirty four", spell);
+        }
     }
 }
